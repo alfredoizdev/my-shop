cd6557e8cf9a2cb8923d4777d8a625d17f290f0f
alfredoizdev <alfredodevelop@gmail.com>
2022-06-12 10:59:33 -0500

    completed order summary using real data

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,6 @@
 export * as db from "./db";
 export * as dbProducts from "./getProducts";
 export * as dbUsers from "./dbUsers";
+export * as dbOrders from "./dbOrders";
 export * from "./constants";
 export * as seedDatabase from "./seed-data";
